84a1004cf70a51af33e1a099e7582ee961a76eff
Bryan Yurasits <git@hexinteractive.net>
2011-10-17 22:13:54 -0400

    refactored the whole thing into an object, getting ride of all those ugly global vars. also added "pause", simply tap the "spacebar". also, added some extra styling. to see these styles add one of these (.round, rings) classes to ul#wall.

--- jcode.js
+++ jcode.js
@@ -1,126 +1,148 @@
 
+var Disco = function(){
+  
+  this.percent = 0;
+	this.lights = 740;
+	this.interval = 100;
+	this.level = 0;
+  this.loop = null;
+  this.wall = null;
+	this.lightsOn = null;
+	this.lightsOff = null;
+	this.dimmer = null;
+	this.hideDimmerTimer = null;
+  this.init();
+};
+
+Disco.prototype.getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+Disco.prototype.createLights = function(num, startOn){
+  
+  return $('<ul id="wall" />').append(function(){
+    // console.log('num', num);      
+    var lights = [];
+    while(num > lights.length){
+      lights.push(document.createElement('li'));
+    }
+    // console.log('num', num);
+    // console.log(lights.length);
+    var classes = ['light']; // 'light ';
+    classes.push(startOn == true ? 'on' : 'off');
+    return $(lights).addClass(classes.join(' '));
+  }).appendTo('body'); 
+};
+
+
+Disco.prototype.setLights = function(){
+  if(this.lightsOn.length <= this.level){
+    this.toggleLight(this.lightsOff);
+  }
+  if(this.lightsOn.length >= this.level){
+    this.toggleLight(this.lightsOn);
+  } 
+};
+
+Disco.prototype.toggleLight = function(list){
+  $(list[ this.getRandomInt(0,list.length) ]).toggleClass('off on');
+};
+
+Disco.prototype.startLoop = function(){
+  var context = this;
+  this.loop = setInterval(function(){context.setLights();},this.interval);
+};
+
+Disco.prototype.stopLoop = function(){
+  clearInterval(this.loop);
+  this.loop = null;
+};
+
+Disco.prototype.init = function(){
+  this.wall = this.createLights(740,false);
+  this.setPercent(60);
+  this.createDimmer();
+  this.lightsOn = document.getElementsByClassName('on');
+  this.lightsOff = document.getElementsByClassName('off');
+  this.bindEvents();
+  this.startLoop();
+};
+
+Disco.prototype.createDimmer = function(){
+  this.dimmer = $('<p id="dimmer"/>').prependTo('body');
+};
+
+Disco.prototype.hideDimmer = function(){
+  var context = this;
+  clearTimeout(context.hideDimmerTimer);
+  this.hideDimmerTimer = setTimeout(function(){context.dimmer.css('opacity','0');},2000);
+};
+
+Disco.prototype.adjustPercent = function(delta){
+  this.setPercent(this.percent + delta);
+};
+
+Disco.prototype.setPercent = function(val){
+  var tempPercent = val;
+  tempPercent = Math.min(tempPercent, 100);
+  tempPercent = Math.max(tempPercent, 0);
+  this.percent = tempPercent;
+  this.setLevel();
+};
+
+Disco.prototype.setLevel = function(){
+  this.level = this.lights * (this.percent/100);
+};
+
+Disco.prototype.bindEvents = function(){
+  var context = this;
+  $(document).keydown(function(event){
+    var caught = true;
+    // console.log('event.keyCode ',event.keyCode);
+    switch(event.keyCode){
+    case 38: // up
+      context.adjustPercent(5);
+    break;
+    case 40: // down
+      context.adjustPercent(-5);
+    break;
+    case 32: //space
+      if(context.loop == null){//paused
+        context.startLoop();//unpause
+      }else{//running
+        context.stopLoop();//pause
+      }
+    break;
+    default:
+      caught = false;
+    break;
+    }
+    
+    if(caught){
+      event.preventDefault();
+      console.log('context.dimmer ',context.dimmer);
+      context.dimmer.text(context.percent).css({
+            'opacity':'0.7',
+            // 'width':context.dimmer.width(),
+            'left':($(window).width()/2) - (context.dimmer.width()/2),
+            'top':($(window).height()/2) - (context.dimmer.height()/2)
+          });
+      context.hideDimmer();
+    }
+  });
+  
+  this.wall.delegate('.light', 'hover', function(){
+    $(this).toggleClass('on off');
+  });
+  
+};
+
 
 $(document).ready(function(){
 
-  function getRandomInt (min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
+  window.disco = new Disco();  
 
-  var createLights = function(num, startOn){
-    
-    return $('<ul id="wall" />').append(function(){
-      // console.log('num', num);      
-      var lights = [];
-      while(num > lights.length){
-        lights.push(document.createElement('li'));
-      }
-      // console.log('num', num);
-      // console.log(lights.length);
-      var classes = ['light']; // 'light ';
-      classes.push(startOn == true ? 'on' : 'off');
-      return $(lights).addClass(classes.join(' ')); //.addClass('on');
-      
-    }).appendTo('body'); 
-  };
-  window.percent = 60;
-  window.lights = 740;
-  // window.level = lights * (percent/100);
-  // window.level = function(){return lights * (percent/100);};
-  window.level = window.lights * (window.percent/100);
-  var lightsOn = document.getElementsByClassName('on');
-  var lightsOff = document.getElementsByClassName('off');
-  var wall = createLights(lights);
-  
-	
-  // var setLights = function(){
-  //   wall.find('.light').each(function(index,elem){
-  //       if(lightsOn.length < level){
-  //         $(this).addClass( Math.round( Math.random() ) ? 'on' : '' );
-  //       }else{
-  //         $(this).removeClass( Math.round( Math.random() ) ? 'on' : '' );
-  //       }
-  // 
-  //     });
-  // };
-  // 
-  //   setInterval(setLights,1000);
-  
-  
-  // var setLights = function(){
-  //   wall.find('.light').eq(getRandomInt(0,wall.find('.light').length) ).each(function(){
-  //       // debugger;
-  //       if(lightsOn.length < level){
-  //         $(this).addClass( Math.round( Math.random() ) ? 'on' : '' );
-  //       }else{
-  //         $(this).removeClass( Math.round( Math.random() ) ? 'on' : '' );
-  //       }
-  // 
-  //     });
-  //     console.log({'level':level, 'lightsOn':lightsOn.length});
-  // };
-  // 
-  //   setInterval(setLights,100);  
-  
-  
-  
-  
-  var setLights = function(){
-    // wall.find('.light').eq(getRandomInt(0,wall.find('.light').length) ).each(function(){
-    //       // todo: make the addClass('on') only happen to 'off' lights and visa versa;
-    //       if(lightsOn.length < level){
-    //         $(this).addClass('on');
-    //       }else{
-    //         $(this).removeClass('on');
-    //       }
-    // 
-    //     });
-    if(lightsOn.length <= level){
-      $(lightsOff[ getRandomInt(0,lightsOff.length) ]).addClass('on').removeClass('off');
-    }
-    if(lightsOn.length >= level){
-      $(lightsOn[ getRandomInt(0,lightsOn.length) ]).addClass('off').removeClass('on');
-    }    
-    // console.log({'level':level(), 'lightsOn':lightsOn.length});
-	};
-	
-  setInterval(setLights,100);
-  
-  
-  $(document).keydown(function(event){
-    switch(event.keyCode){
-    case 38: // up
-      percent = Math.min(percent + 5, 100);
-    break;
-    case 40: // down
-      percent = Math.max(percent - 5, 0);
-    break;
-    }
-    console.log('percent',percent);
-    
-    window.level = window.lights * (window.percent/100);
-    if($('#pct').length == 0){
-      $('<p id="pct"/>').appendTo('body');
-    }
-    clearTimeout(window.hideControls);
-    var maggie = $('#pct');
-    $('#pct').text(window.percent).css({
-          'opacity':'0.5'//,
-          // 'width':maggie.width(),
-          // 'margin':'0 auto',
-          // 'top':($(window).css('height')/2) - (maggie.css('height')/2)
-        });
-    window.hideControls = setTimeout(function(){$('#pct').css('opacity','0');},2000);
-    
-    
-  });
-  
-  wall.delegate('.light', 'hover', function(){
-    $(this).toggleClass('on');
-  });
-  
-	
-});//end of ready
+});
 
 
 
